d42348fe642d4c8cc660e34f6d73804a74e87c95
manowjeed <manowjeed@hotmail.com>
2020-08-25 22:24:49 +0700

    Update Main.php
    เปลี่ยน return view เป็นไฟล์ logout

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -9,7 +9,7 @@ class Main extends BaseController
     
     public function logout()
 	{
-		return view('welcome_message');
+		return view('logout');
 	}
 
 	//--------------------------------------------------------------------
